b2b6c39a517a9422c10d1789fc21c6b2088dea79
vamshicherala <vamshikrishna199904@gmail.com>
2021-10-13 19:53:13 +0530

    Added:Welcome msg to EmployeeWage file

---
+++ src/com/bridgelab/EmployeeWageBuilder.java
@@ -0,0 +1,7 @@
+package com.bridgelab;
+
+public class EmployeeWageBuilder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program On Master Branch");
+    }
+}
