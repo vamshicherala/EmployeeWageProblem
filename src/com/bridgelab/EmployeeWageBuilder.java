c6fdbb695931de775f4cc3166c6c781e13053f1f
vamshicherala <vamshikrishna199904@gmail.com>
2021-10-13 21:44:54 +0530

    Added:checking emp is present or absent

--- src/com/bridgelab/EmployeeWageBuilder.java
+++ src/com/bridgelab/EmployeeWageBuilder.java
@@ -1,7 +1,18 @@
 package com.bridgelab;
 
+import java.util.Random;
+
 public class EmployeeWageBuilder {
+    public static final int IS_PRESENT = 1;
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program On Master Branch");
+        System.out.println("**Welcome to Employee Wage Computation Program On Master Branch**");
+        Random rand = new Random();
+        int empcheck = (int) (rand.nextFloat()*10)%2;
+        if (empcheck == IS_PRESENT) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+
     }
 }
